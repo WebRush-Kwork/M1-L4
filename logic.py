dd07e4ee07215f353d09fb7b21486fd91f92325a
gleb <webrushgithub@gmail.com>
2023-11-26 19:45:20 +0600

    Добавил 3 новые функции

--- logic.py
+++ logic.py
@@ -11,8 +11,9 @@ class Pokemon:
         self.pokemon_trainer = pokemon_trainer
 
         self.pokemon_number = randint(1, 1000)
-        self.img = self.get_img()
+        self.img = self.get_full_image()
         self.name = self.get_name()
+        self.hp = randint(1, 100)
 
         Pokemon.pokemons[pokemon_trainer] = self
 
@@ -26,14 +27,41 @@ class Pokemon:
         response = requests.get(url)
         if response.status_code == 200:
             data = response.json()
-            return (data['forms'][0]['name'])
+            return (data['name'])
         else:
             return 'Pikachu'
+
+    def get_front_image(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}/'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['sprites']['front_default'])
+        else:
+            return 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fanime-characters-fight.fandom.com%2Fru%2Fwiki%2F%25D0%259F%25D0%25B8%25D0%25BA%25D0%25B0%25D1%2587%25D1%2583_%25D0%25AD%25D1%2588%25D0%25B0&psig=AOvVaw1x8ss28AIPpm2Huin42RCo&ust=1701087406215000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCLCZwMjS4YIDFQAAAAAdAAAAABAE'
+
+    def get_back_image(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}/'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['sprites']['back_default'])
+        else:
+            return 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fanime-characters-fight.fandom.com%2Fru%2Fwiki%2F%25D0%259F%25D0%25B8%25D0%25BA%25D0%25B0%25D1%2587%25D1%2583_%25D0%25AD%25D1%2588%25D0%25B0&psig=AOvVaw1x8ss28AIPpm2Huin42RCo&ust=1701087406215000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCLCZwMjS4YIDFQAAAAAdAAAAABAE'
+
+    def get_full_image(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}/'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['sprites']['other']['official-artwork']['front_default'])
+        else:
+            return 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fanime-characters-fight.fandom.com%2Fru%2Fwiki%2F%25D0%259F%25D0%25B8%25D0%25BA%25D0%25B0%25D1%2587%25D1%2583_%25D0%25AD%25D1%2588%25D0%25B0&psig=AOvVaw1x8ss28AIPpm2Huin42RCo&ust=1701087406215000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCLCZwMjS4YIDFQAAAAAdAAAAABAE'
 
     # Метод класса для получения информации
 
     def info(self):
-        return f'Имя твоего покеомона: {self.name}'
+        return f'Тренер покемона: @{self.pokemon_trainer}\nИмя твоего покеомона: {self.name}\nЗдоровье покемона: {self.hp}%'
 
     # Метод класса для получения картинки покемона
     def show_img(self):
