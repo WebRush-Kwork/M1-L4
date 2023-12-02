3a180d1996d24e4c51bfac89f815a240ac2fd274
gleb <webrushgithub@gmail.com>
2023-12-02 14:49:01 +0600

    Доработал функцию сохранения покемона для пользователя

--- main.py
+++ main.py
@@ -11,12 +11,27 @@ def start(message):
     bot.send_message(message.chat.id, 'Здравствуйте! 🔥\nЭто бот для создания своего собственного покемона! ✍️\nСписок доступных команд Вы можете увидеть рядом с кнопкой отправки сообщения! 💻')
 
 
+def get_logged_users():
+    try:
+        with open('logged_users.txt', 'r') as file:
+            return file.read().splitlines()
+    except FileNotFoundError:
+        return []
+
+
+logged_users = get_logged_users()
+
+
 @bot.message_handler(commands=['create'])
 def create(message):
-    if message.from_user.username not in Pokemon.pokemons.keys():
+    global logged_users
+    if message.from_user.username not in logged_users:
         pokemon = Pokemon(message.from_user.username)
         bot.send_message(message.chat.id, pokemon.info())
         bot.send_photo(message.chat.id, pokemon.show_img())
+        with open('logged_users.txt', 'a') as file:
+            file.write(f'{message.from_user.username}\n')
+        logged_users.append(message.from_user.username)
     else:
         bot.reply_to(
             message, 'Вы уже создали себе покемона! Посмотреть его можете по команде /pokemon 💻')
@@ -24,7 +39,7 @@ def create(message):
 
 @bot.message_handler(commands=['pokemon'])
 def pokemon(message):
-    if message.from_user.username in Pokemon.pokemons.keys():
+    if message.from_user.username in logged_users and Pokemon.pokemons.keys():
         pokemon = Pokemon.pokemons[message.from_user.username]
         bot.send_message(message.chat.id, pokemon.info())
         bot.send_photo(message.chat.id, pokemon.show_img())
